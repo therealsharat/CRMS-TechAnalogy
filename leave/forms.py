68a4dd152b8e4476e2faf16608aed1de65da89bd
nikhil <ravipatinikhil71@gmail.com>
2022-01-10 01:12:30 +0530

    Configured Leave model and integrated with Leave.html

---
+++ leave/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import *
+CHOICES=(('sick','sick'),('casual','casual'),('emergency','emergency'),("study",'study'))
+class LeaveCreationForm(forms.ModelForm):
+    startdate=forms.DateField()
+    enddate=forms.DateField()
+    reason=forms.CharField(max_length=100)
+    leavetype=forms.ChoiceField(choices = CHOICES,
+                       widget=forms.Select(attrs={'class': 'nice-select rounded'}),
+                       )
+    class Meta:
+        model =leave
+        fields=['startdate','enddate','reason','leavetype']
