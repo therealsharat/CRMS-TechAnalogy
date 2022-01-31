2f8930b79be71347ebf5d0688a42af030bc4e74c
nikhil <ravipatinikhil71@gmail.com>
2022-02-01 00:26:25 +0530

    added tokens

--- Billion_Dollar_Code/settings.py
+++ Billion_Dollar_Code/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG == False:
     ALLOWED_HOSTS = [env('ALLOWED_HOST_1'),'.tech-analogy.engineer',env('ALLOWED_HOST_2')]
 
