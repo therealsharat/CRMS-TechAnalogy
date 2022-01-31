3a48b94d53b77e9245723596a88ffe83eb729bb7
Ubuntu <ubuntu@ip-172-31-38-243.ap-south-1.compute.internal>
2022-01-31 14:15:39 +0000

    updated employee details and added domain server

--- Billion_Dollar_Code/settings.py
+++ Billion_Dollar_Code/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [env('ALLOWED_HOST_1')]
+ALLOWED_HOSTS = [env('ALLOWED_HOST_1'),'.tech-analogy.engineer',env('ALLOWED_HOST_2')]
 
 
 # Application definition
